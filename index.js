12521fb629a4316f1b6b07f203320f143a598fb5
SeanQuinn <Seanquinn917@gmail.com>
2023-01-20 18:04:38 -0500

    commit tree db996d1abc4852ead2a19201366be9a71094bfc2

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Add your code here
+
+const moreData = {
+    name: "Steve",
+    email: "steve@steve.com"
+};
+
+
+  
+function submitData(string1, string2){
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: string1,
+            email: string2
+        }),
+      };
+   return fetch("http://localhost:3000/users", configurationObject)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(Object){
+    console.log(Object)
+    document.body.innerHTML =(Object.id);
+  })    
+    .catch(function(errorObject){
+       document.body.innerHTML = (errorObject.message);
+    })
+    }
+
+    const newInnerHTML = {
+        name: "Jim",
+        email: "jim@jim.com"
+    }
+
+  
